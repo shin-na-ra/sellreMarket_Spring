774dc12341c637e2cb47d4f012e74f8017c0c67e
shin-na-ra <nas221@naver.com>
2024-02-27 12:24:46 +0900

    2024.02.27 snr : 제품 생성

---
+++ SellreMarket/src/main/java/com/springlec/base/service/AdminProductServiceImpl.java
@@ -0,0 +1,201 @@
+package com.springlec.base.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.springlec.base.dao.AdminProductDao;
+import com.springlec.base.model.AdminDeliveryDto;
+import com.springlec.base.model.AdminPackDto;
+import com.springlec.base.model.AdminPageDto;
+import com.springlec.base.model.AdminProductDto;
+import com.springlec.base.model.BrandDto;
+import com.springlec.base.model.Category;
+
+@Service
+@Transactional(rollbackFor = Exception.class)
+public class AdminProductServiceImpl implements AdminProductService {
+
+	int pageLimit = 15;
+	int blockLimit = 5;
+	
+	@Autowired
+	AdminProductDao dao;
+	
+	@Autowired
+	AdminPageDto dto;
+
+	@Override
+	public List<AdminProductDto> list(int page) throws Exception {
+		int pageStart = (page - 1) * pageLimit + 1;
+		return dao.list(pageStart);
+	}
+
+	@Override
+	public AdminPageDto pagingParam(int page) throws Exception {
+		int boardCount = dao.listCount();
+		
+		int maxPage = (int) (Math.ceil((double) boardCount / pageLimit));
+		
+		//시작페이지 계산
+		int startPage = (int)(Math.ceil((double) page / blockLimit) - 1) * blockLimit + 1;
+		
+		int endPage = startPage + blockLimit -1;
+		if(endPage > maxPage) {
+			endPage = maxPage;
+		}
+		
+		dto.setPage(page);
+		dto.setStartPage(startPage);
+		dto.setMaxPage(maxPage);
+		dto.setEndPage(endPage);
+		
+		return dto;
+	}
+	
+	@Override
+	public int listCount() throws Exception {
+		return dao.listCount();
+	}
+
+	@Override
+	public List<BrandDto> brandList() throws Exception {
+		return dao.brandList();
+	}
+
+	@Override
+	public List<Category> categoryList() throws Exception {
+		return dao.categoryList();
+	}
+
+	@Override
+	public List<Category> subCategoryList(String type) throws Exception {
+		return dao.subCategoryList(type);
+	}
+
+	@Override
+	public List<AdminPackDto> selectPackType() throws Exception {
+		return dao.selectPackType();
+	}
+
+	@Override
+	public List<AdminPackDto> selectPackKind() throws Exception {
+		return dao.selectPackKind();
+	}
+
+	@Override
+	public List<AdminDeliveryDto> selectDelivery() throws Exception {
+		return dao.selectDelivery();
+	}
+
+	@Override
+	public void insertInfo(String pname, String pEngname, String allery, String nutrition, int pstock, String origin,
+			String description, int price, String bname, String subtype, String type, String packkind, String packtype,
+			String utype, String ugram, String dname) throws Exception {
+		
+		
+		//제품 등록하기
+		dao.productInsert(pname, pEngname, allery, nutrition, pstock, origin, description);
+		
+		//등록된 제품 가져오기
+		int productid = dao.getProductId(pname, pEngname, allery);
+		
+		//가격 등록
+		dao.insertPrice(price, productid);
+		
+		//브랜드 등록
+		dao.insertBrand(productid, bname);
+		
+		//카테고리 등록
+		dao.insertCategory(productid, type, subtype);
+		
+		//포장타입, 종류 등록
+		dao.insertPack(productid, packtype, packkind);
+		
+		//단위 등록
+		dao.insertUnit(productid, utype, ugram);
+		
+		dao.insertDelivery(productid, dname);
+	}
+
+	@Override
+	public List<Category> selectSubCategory() throws Exception {
+		return dao.selectSubCategory();
+	}
+
+	@Override
+	public List<AdminProductDto> listQuery(String search, int page) throws Exception {
+		search = '%'+search+'%';
+		int pageStart = (page - 1) * pageLimit + 1;
+		return dao.listQuery(search, pageStart);
+	}
+
+	@Override
+	public int searchCount(String search) throws Exception {
+		search = '%'+search+'%';
+		return dao.searchCount(search);
+	}
+	
+	@Override
+	public AdminPageDto pagingParam2(int page, String search) throws Exception {
+		search = '%'+search+'%';
+		int boardCount = dao.searchCount(search);
+		
+		int maxPage = (int) (Math.ceil((double) boardCount / pageLimit));
+		
+		//시작페이지 계산
+		int startPage = (int)(Math.ceil((double) page / blockLimit) - 1) * blockLimit + 1;
+		
+		int endPage = startPage + blockLimit -1;
+		if(endPage > maxPage) {
+			endPage = maxPage;
+		}
+		
+		dto.setPage(page);
+		dto.setStartPage(startPage);
+		dto.setMaxPage(maxPage);
+		dto.setEndPage(endPage);
+		
+		return dto;
+	}
+
+	@Override
+	public List<AdminProductDto> detail(int productid) throws Exception {
+		return dao.detail(productid);
+	}
+
+	@Override
+	public void updateInfo(String pname, String pEngname, String allery, String nutrition, int pstock, String origin,
+			String description, int price, String bname, String subtype, String type, String packkind, String packtype,
+			String utype, String ugram, String dname, int productid) throws Exception {
+		
+		//제품 등록하기
+		dao.productUpdate(pname, pEngname, allery, nutrition, pstock, origin, description, productid);
+		
+		//가격 등록
+		dao.updatePrice(price, productid);
+		
+		//브랜드 등록
+		dao.updateBrand(productid, bname);
+		
+		//카테고리 등록
+		dao.updateCategory(productid, type, subtype);
+		
+		//포장타입, 종류 등록
+		dao.updatePack(productid, packtype, packkind);
+		
+		//단위 등록
+		dao.updateUnit(productid, utype, ugram);
+		
+		dao.updateDelivery(productid, dname);
+		
+	}
+
+	@Override
+	public void delete(int productid) throws Exception {
+		dao.delete(productid);
+	}
+	
+}
