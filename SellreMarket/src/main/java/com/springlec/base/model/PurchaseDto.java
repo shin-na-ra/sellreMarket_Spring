ad96513a9f68fc7a45691e0cd227439b42f61bd6
hwicoding <dlgnl117@gmail.com>
2024-02-28 09:51:43 +0900

    24.02.28 구매 페이지 검색기능 추가

---
+++ SellreMarket/src/main/java/com/springlec/base/model/PurchaseDto.java
@@ -0,0 +1,65 @@
+package com.springlec.base.model;
+
+import java.sql.Date;
+import java.sql.Timestamp;
+
+public class PurchaseDto {
+	
+	int purchasePrice, purchaseAmount;
+	String purchaseId, productImage, productName;
+	Date purchaseDate;
+	
+	public PurchaseDto() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public int getPurchasePrice() {
+		return purchasePrice;
+	}
+
+	public void setPurchasePrice(int purchasePrice) {
+		this.purchasePrice = purchasePrice;
+	}
+
+	public int getPurchaseAmount() {
+		return purchaseAmount;
+	}
+
+	public void setPurchaseAmount(int purchaseAmount) {
+		this.purchaseAmount = purchaseAmount;
+	}
+
+	public String getPurchaseId() {
+		return purchaseId;
+	}
+
+	public void setPurchaseId(String purchaseId) {
+		this.purchaseId = purchaseId;
+	}
+
+	public String getProductImage() {
+		return productImage;
+	}
+
+	public void setProductImage(String productImage) {
+		this.productImage = productImage;
+	}
+
+	public String getProductName() {
+		return productName;
+	}
+
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+
+	public Date getPurchaseDate() {
+		return purchaseDate;
+	}
+
+	public void setPurchaseDate(Date purchaseDate) {
+		this.purchaseDate = purchaseDate;
+	}
+
+	
+}
